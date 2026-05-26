9ba5d9f0c8de91508085977a8cf4a98bdf91023f
hywznn <choihj0510@naver.com>
2026-05-26 11:12:07 +0900

    260526 > Components > feat : BookCard 컴포넌트 제작

--- src/components/bookCard/BookImage.jsx
+++ src/components/bookCard/BookImage.jsx
@@ -1,2 +1,17 @@
-function BookImage() {}
+import "./BookImageStyle.css";
+
+function BookImage({ src, alt = "도서 표지" }) {
+  return (
+    <div className="book-image-box">
+      {src ? (
+        // 생성된 이미지
+        <img className="book-image" src={src} alt={alt} />
+      ) : (
+        // 기본 이미지 
+        <div className="book-image-placeholder">No Image</div>
+      )}
+    </div>
+  );
+}
+
 export default BookImage;
