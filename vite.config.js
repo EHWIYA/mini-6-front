ada6d7072f154ac5e098e87b15312fe2fdd22894
EHWIYA <150232499+EHWIYA@users.noreply.github.com>
2026-06-24 13:09:47 +0900

    fix: 백엔드 주소 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const backendUrl = 'http://47.128.222.99:8080'
+const backendUrl = 'http://team-14-backend-alb-256967163.ap-southeast-1.elb.amazonaws.com'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
